f0a532c735fe39c3cd994ea2099f0b641259d267
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-10-20 14:13:17 +0100

    Updated

--- src/services/authors/index.js
+++ src/services/authors/index.js
@@ -1,25 +1,44 @@
-import express from "express"
+import express from "express";
+import fs from "fs";
+import uniqid from "uniqid";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
-const authorRouter = express.Router()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const authorsFilePath = path.join(__dirname, "authors.json");
+const authorRouter = express.Router();
 
-authorRouter.post("/", (req, res) => {
-    res.send("Post")
-})
+authorRouter.post("/:id", (req, res, next) => {
+    try {
 
-authorRouter.get("/", (req, res) => {
-    res.send("Get")
-})
+    } catch (error) {
+        res.send(500).send({ message: error.message });
+    }
+});
 
-authorRouter.get("/:id", (req, res) => {
-    res.send("Get")
-})
+authorRouter.get("/", (req, res, next) => {
+    try {
 
-authorRouter.put("/:id", (req, res) => {
+    } catch (error) {
+        res.send(500).send({ message: error.message });
+    }
+});
+
+authorRouter.get("/:id", (req, res, next) => {
+    try {
+
+    } catch (error) {
+        res.send(500).send({ message: error.message });
+    }
+});
+
+authorRouter.put("/:id", (req, res, next) => {
     res.send("Put")
-})
+});
 
-authorRouter.delete("/:id", (req, res) => {
+authorRouter.delete("/:id", (req, res, next) => {
     res.send("Delete")
-})
+});
 
-export default authorRouter
+export default authorRouter;
